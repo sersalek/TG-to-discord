ab3e68ac30464b39b6cb71d70e7066a747b51abe
SerSaLek <mnxu608@gmail.com>
2022-09-11 19:11:01 +0430

    add option use_proxy

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import python_socks
 
 api_id = 111111
 api_hash = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+use_proxy = False
 proxy_type = connection.ConnectionTcpMTProxyRandomizedIntermediate
 # proxy_type = None
 proxy_settings = ('cloudflare.com.nokia.com.co.uk.do_yo.want_to.clash_with.this.www.microsoft.com.there_is_no.place_like.localhost.www.bing.com.count_with_me.cyou.com.now_sudo.rm_rf.ddns.net.we_are_here.again_to_fight.everyone.i_am.the_internet.special_wayoka.tavalodi.cfd.', 443, 'dd00000000000000000000000000000000')
--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import os
 from discord_webhook import DiscordWebhook
 from telethon import TelegramClient, events
 try:
-    import private_config
+    import private_config as config
 except ModuleNotFoundError:
-    import config as private_config
+    import config
 
-client = TelegramClient('session', private_config.api_id, private_config.api_hash,connection=private_config.proxy_type,proxy=private_config.proxy_settings)
+if config.use_proxy:
+    client = TelegramClient('session', config.api_id, config.api_hash,connection=config.proxy_type,proxy=config.proxy_settings)
+else:
+    client = TelegramClient('session', config.api_id, config.api_hash)
 
 @client.on(events.NewMessage)
 async def my_event_handler(message):
     chat = await message.get_chat()
-    if chat.title == private_config.channel_name or message.chat_id == private_config.channel_id:
-        webhook = DiscordWebhook(url=private_config.discord_webhook)
-        webhook.avatar_url = private_config.avatar_url
-        webhook.username = private_config.username
+    if chat.title == config.channel_name or message.chat_id == config.channel_id:
+        webhook = DiscordWebhook(url=config.discord_webhook)
+        webhook.avatar_url = config.avatar_url
+        webhook.username = config.username
         webhook.content = message.raw_text
         if message.photo:
             path = await client.download_media(message.media)
